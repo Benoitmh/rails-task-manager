c2c6765e33c5fbc9fc115bd11081c26899d061fe
mahe <benoitmahe5@gmail.com>
2022-11-15 15:50:32 +0100

    add New, Edit and Delete task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: 'new_task' # page form
+  post 'tasks', to: 'tasks#create' # url receive data from form
+
+  # READ
   get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
